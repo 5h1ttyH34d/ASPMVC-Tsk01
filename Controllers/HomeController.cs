eab5525686abf9c2f057a809acdcbe30a2387b36
K33kO53R <k33kO5@outlook.com>
2022-03-10 13:01:24 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPMVC_Tsk01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Main Page";
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/WorkersController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ASPMVC_Tsk01.Models;
+
+namespace ASPMVC_Tsk01.Controllers
+{
+    public class WorkersController : Controller
+    {
+        private SomeDBEntities db = new SomeDBEntities();
+
+        // GET: Workers
+        public ActionResult Index()
+        {
+            var workers = db.Workers.Include(w => w.Buildings);
+            return View(workers.ToList());
+        }
+
+        // GET: Workers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Workers workers = db.Workers.Find(id);
+            if (workers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(workers);
+        }
+
+        // GET: Workers/Create
+        public ActionResult Create()
+        {
+            ViewBag.BuildingId = new SelectList(db.Buildings, "Id", "Adress");
+            return View();
+        }
+
+        // POST: Workers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,PatrName,PhoneNumber,Speciality,BuildingId")] Workers workers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Workers.Add(workers);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.BuildingId = new SelectList(db.Buildings, "Id", "Adress", workers.BuildingId);
+            return View(workers);
+        }
+
+        // GET: Workers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Workers workers = db.Workers.Find(id);
+            if (workers == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.BuildingId = new SelectList(db.Buildings, "Id", "Adress", workers.BuildingId);
+            return View(workers);
+        }
+
+        // POST: Workers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,PatrName,PhoneNumber,Speciality,BuildingId")] Workers workers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(workers).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BuildingId = new SelectList(db.Buildings, "Id", "Adress", workers.BuildingId);
+            return View(workers);
+        }
+
+        // GET: Workers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Workers workers = db.Workers.Find(id);
+            if (workers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(workers);
+        }
+
+        // POST: Workers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Workers workers = db.Workers.Find(id);
+            db.Workers.Remove(workers);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
